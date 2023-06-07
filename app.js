3b0fc293988e20d526b123ea30461816333afca4
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-06-07 10:49:39 +0530

    added the database for new user

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://0.0.0.0:27017/userdb",{useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = mongoose.model("User",userSchema);
 
 app.get('/', (req, res) => {
     res.render("home");
@@ -20,6 +30,20 @@ app.get("/login", (req, res)=>{
 app.get("/register", (req, res)=>{
     res.render("register");
 });
+
+app.post("/register",(req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render("secrets");
+        }
+    })
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
