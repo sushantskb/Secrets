09c1e1ba479b7c936a60fc48407bb3229c14b4b2
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-06-07 11:07:06 +0530

    added the login route

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//connection
 mongoose.connect("mongodb://0.0.0.0:27017/userdb",{useNewUrlParser: true});
 
+//user schema
 const userSchema = {
     email: String,
     password: String
 };
 
+//mongoose model
 const User = mongoose.model("User",userSchema);
 
 app.get('/', (req, res) => {
@@ -31,6 +34,7 @@ app.get("/register", (req, res)=>{
     res.render("register");
 });
 
+//register new user
 app.post("/register",(req, res)=>{
     const newUser = new User({
         email: req.body.username,
@@ -42,8 +46,28 @@ app.post("/register",(req, res)=>{
         } else{
             res.render("secrets");
         }
-    })
-})
+    });
+});
 
+//login route
+app.post("/login", (req, res)=>{
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: userName},(err, foundUser)=>{
+        if(err){
+            console.log(err);
+        } else 
+            if(foundUser){
+                if(foundUser.password === password){
+                res.render("secrets");
+            }
+        }
+    });
+});
+
+app.get("/logout",(req, res)=>{
+    res.render("home");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
